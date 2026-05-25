102a4b3ce7c3351382a5382a36fe47eb337ec8c8
sudeen28 <polygrammy28@gmail.com>
2026-05-25 20:46:28 +0100

    v

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 
 // ---- Middleware ----
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
+  origin: "*",
   credentials: true,
 }));
 app.use(express.json());
